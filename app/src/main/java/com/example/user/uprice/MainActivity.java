cc58d0028f9b229f22ebb26acbc6e7d1cc7ba595
abc873693 <abc873693@gmail.com>
2016-12-18 22:21:45 +0800

    test okhttp

--- app/src/main/java/com/example/user/uprice/MainActivity.java
+++ app/src/main/java/com/example/user/uprice/MainActivity.java
@@ -2,20 +2,32 @@ package com.example.user.uprice;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    public static final MediaType MEDIA_TYPE_MARKDOWN
+            = MediaType.parse("application/soap+xml; charset=utf-8");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +52,38 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        String postBody = "<soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\">" +
+                "  <soap12:Body>" +
+                "    <getCPCMainProdListPrice_English xmlns=\"http://tmtd.cpc.com.tw/\" />\n" +
+                "  </soap12:Body>" +
+                "</soap12:Envelope>";
+        RequestBody body = RequestBody.create(MEDIA_TYPE_MARKDOWN, postBody);
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("http://vipmember.tmtd.cpc.com.tw/OpenData/ListPriceWebService.asmx")
+                .post(body)
+                .addHeader("Content-Type", "application/soap+xml; charset=utf-8") //Notice this request has header if you don't need to send a header just erase this part
+                .addHeader("Content-Length", "length")
+                .build();
+
+        Call call = client.newCall(request);
+
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e("HttpService", "onFailure() Request was: " );
+
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String str = response.body().string();
+                Log.e("response ", "onResponse(): " + str );
+            }
+        });
+
     }
 
     @Override
